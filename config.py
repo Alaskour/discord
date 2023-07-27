107ce0ad647ecdb0def5ffbbb6ce3bcef8c64ccd
Alaskour <105124056+Alaskour@users.noreply.github.com>
2023-07-27 22:27:12 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+BOT_TOKEN = "MTEzMzc1ODkxMjMwMzIwMjMwNA.Gdelzu.nM4Cme7DTnpYmlcvs_5naSZ8wFRcayCirxBXBA"
+ID_POST = 1134151687897096303  # Your Post ID
+USER_ROLES_LIST = ()
+MAX_ROLES = 10
+
+ROLES_LIST = {  # Your roles ID and emoji
+    "✅": 1133285817259937873,  # CS:GO
+    "🤬": 1133284747594960938,  # Rust
+    "🤡": 1133287992425988116,  # Dota
+    "❓": 1133289349249773630,  # GTA V
+    "☑️": 1133336463707217961,  # Valorant
+    "🏅": 1134138725266374706,  # Minecraft
+}
